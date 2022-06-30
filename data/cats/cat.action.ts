fe0bd056c6c90fc824a45a337f26cb6749bb0f24
lauchacarro <lautaroecarro@gmail.com>
2022-06-29 23:16:29 -0300

    2 files modified

--- data/cats/cat.action.ts
+++ data/cats/cat.action.ts
@@ -4,6 +4,7 @@ import { getCatsData } from './cat.api';
 
 export const loadCats =
   () => async (dispatch: React.Dispatch<DispatchObject>) => {
+    console.log('Antes del dispatch', dispatch);
     dispatch(setLoading(true));
 
     const cats = await getCatsData();
@@ -12,11 +13,34 @@ export const loadCats =
     dispatch(setLoading(false));
   };
 
-export const setLoading = (isLoading: boolean) =>
-  ({
+  export const loadCatsVacio =
+  () => (dispatch: React.Dispatch<DispatchObject>) => {
+    console.log('Antes del dispatch', dispatch);
+    dispatch(setLoading(true));
+
+    const cats = [{ "id": "595f280c557291a9750ebf80",
+          "created_at": "2015-11-06T18:36:37.342Z",
+          "tags": [
+            "cute",
+            "eyes"
+    ]}];
+    // dispatch(setCats(cats));
+
+    dispatch(setLoading(false));
+
+    return ({
+      type: 'set-cats',
+      cats,
+    } as DispatchObject)
+  };
+
+export const setLoading = (isLoading: boolean) => {
+  console.log('Antes del dispatch', isLoading);
+  return ({
     type: 'set-loading',
     isLoading,
   } as DispatchObject);
+}
 
 export const setCats = (cats: Cat[]) =>
   ({
--- data/wrapAsync.ts
+++ data/wrapAsync.ts
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Dispatch } from 'react';
-import { ActionType, DispatchObject } from '../utils/types';
-import { AppContext } from './AppContext';
+import { DispatchObject } from '../utils/types';
 
 type FuncType = (dispatch: React.Dispatch<any>) => Promise<DispatchObject>;
 
@@ -13,42 +12,49 @@ function isObjectAction(
   return (<DispatchObject>dispatchFunc)?.type !== undefined;
 }
 
-function isFuncType(dispatchFunc: any): dispatchFunc is FuncType {
-  return (<FuncType>dispatchFunc).name !== undefined;
+function isPromise(obj: any) {
+  return typeof obj === 'object' && obj.then;
+}
+
+function isAsyncFunction(obj: any) {
+  return obj.constructor?.name === 'AsyncFunction';
+}
+
+function isFunction(obj: any) {
+  return typeof obj === 'function';
 }
 
 export function wrapAsync(dispatch: Dispatch<DispatchObject>) {
-  return (func: any, ...args: any) => {
+  const dispatchAsync = (func: any, ...args: any) => {
     if (isObjectAction(func)) {
       dispatch(func);
       return;
     }
 
-    if (isFuncType(func)) {
-      resolveDispatchFunc(func, dispatch);
-    }
+    if (isFunction(func)) {
+      if (isPromise(func)) {
+        resolvePromise(func, dispatch);
+      } else if (isAsyncFunction(func)) {
+        const result = func(dispatch);
+        if (isPromise(result)) {
+          resolvePromise(result, dispatch);
+        }
+      } else {
+        const result = args?.length ? func(...args) : func(dispatch);
 
-    const dispatchFunc = func(...args);
-
-    if (isObjectAction(dispatchFunc)) {
-      dispatch(dispatchFunc);
-      return;
-    }
-
-    if (typeof dispatchFunc === 'function') {
-      resolveDispatchFunc(dispatchFunc, dispatch);
+        if (result) {
+          dispatchAsync(result, dispatch);
+        }
+      }
     }
   };
+  return dispatchAsync;
 }
 
-function resolveDispatchFunc(dispatchFunc: FuncType, dispatch: Dispatch<any>) {
-  const result = dispatchFunc(dispatch);
-
-  if (typeof result === 'object' && result.then) {
-    result.then((dispatchObject?: DispatchObject) => {
-      if (dispatchObject && dispatchObject.type) {
-        dispatch(dispatchObject);
-      }
-    });
-  }
+function resolvePromise(promise: Promise<any>, dispatch: Dispatch<any>) {
+  promise.then((dispatchObject?: DispatchObject) => {
+    if (dispatchObject && dispatchObject.type) {
+      dispatch(dispatchObject);
+    }
+  });
 }
