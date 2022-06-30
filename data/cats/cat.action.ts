f31b02101446f18b020f53c6ee356738f762a4fb
lauchacarro <lautaroecarro@gmail.com>
2022-06-29 23:17:32 -0300

    1 file modified

--- data/cats/cat.action.ts
+++ data/cats/cat.action.ts
@@ -4,7 +4,6 @@ import { getCatsData } from './cat.api';
 
 export const loadCats =
   () => async (dispatch: React.Dispatch<DispatchObject>) => {
-    console.log('Antes del dispatch', dispatch);
     dispatch(setLoading(true));
 
     const cats = await getCatsData();
@@ -13,34 +12,11 @@ export const loadCats =
     dispatch(setLoading(false));
   };
 
-  export const loadCatsVacio =
-  () => (dispatch: React.Dispatch<DispatchObject>) => {
-    console.log('Antes del dispatch', dispatch);
-    dispatch(setLoading(true));
-
-    const cats = [{ "id": "595f280c557291a9750ebf80",
-          "created_at": "2015-11-06T18:36:37.342Z",
-          "tags": [
-            "cute",
-            "eyes"
-    ]}];
-    // dispatch(setCats(cats));
-
-    dispatch(setLoading(false));
-
-    return ({
-      type: 'set-cats',
-      cats,
-    } as DispatchObject)
-  };
-
-export const setLoading = (isLoading: boolean) => {
-  console.log('Antes del dispatch', isLoading);
-  return ({
+export const setLoading = (isLoading: boolean) =>
+  ({
     type: 'set-loading',
     isLoading,
   } as DispatchObject);
-}
 
 export const setCats = (cats: Cat[]) =>
   ({
